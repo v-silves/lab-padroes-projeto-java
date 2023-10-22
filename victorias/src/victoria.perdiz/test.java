15c05928655c09949489a98efd6e70594ab8c207
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-22 14:52:27 -0300

    Create test.java
    Reproduzindo teste de padrão de projeto.

---
+++ victorias/src/victoria.perdiz/test.java
@@ -0,0 +1,8 @@
+package victoria.perdiz
+
+public class Test {
+	public static void main (String[] args) {
+		ImitadorDevagar devagar = ImitadorDevagar.getInstancia();
+		System.out.printIn(devagar);
+	}
+}
